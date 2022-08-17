1c4ecbcb3080c1c9687a0a1da80e3cdcbe9e77e5
rohitsharma0304 <100235097+rohitsharma0304@users.noreply.github.com>
2022-08-16 21:17:18 -0700

    Update key-pair-main.tf

--- key-pair-main.tf
+++ key-pair-main.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "key_pair" {
 }
 # Create the Key Pair
 resource "aws_key_pair" "key_pair" {
-  key_name   = "kopicloud-key-pair"  
+  key_name   = "ztp-key-pair"  
   public_key = tls_private_key.key_pair.public_key_openssh
 }
 # Save file
